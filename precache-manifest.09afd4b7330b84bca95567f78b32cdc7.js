8b7343a7433f1aa7cefd57e5bd44bd00c6a0fcdf
edward-kabue <ekabue0@gmail.com>
2020-10-24 15:09:11 +0300

    Updates

---
+++ precache-manifest.09afd4b7330b84bca95567f78b32cdc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7bcd76bba679468dd07f49de10446ad",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b9d2b500472dd8cef1c6",
+    "url": "/monster-rolodex/static/css/main.f6c26626.chunk.css"
+  },
+  {
+    "revision": "5a9b1843109f55a1e93a",
+    "url": "/monster-rolodex/static/js/2.f35561e9.chunk.js"
+  },
+  {
+    "revision": "b9d2b500472dd8cef1c6",
+    "url": "/monster-rolodex/static/js/main.2b9a5139.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
